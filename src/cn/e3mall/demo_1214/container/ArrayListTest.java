94c8c587b8cd93ee3869ab0f542bcad697caa9de
fury36 <40300926+fury36@users.noreply.github.com>
2019-12-19 11:42:49 +0800

    测试ArrayList的不安全性

---
+++ src/cn/e3mall/demo_1214/container/ArrayListTest.java
@@ -0,0 +1,33 @@
+package cn.e3mall.demo_1214.container;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * <p>
+ *     测试ArrayList的不安全性
+ * </p>
+ * @ClassName: ArrayListTest
+ * @Auther: admin
+ * @Date: 2019/12/18 15:31
+ * @Description:
+ */
+public class ArrayListTest {
+
+    private static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        // 验证arraylist的不安全性
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString());
+            }).start();
+        }
+
+        Thread.sleep(1000);
+
+        System.out.println(list);
+    }
+
+}
