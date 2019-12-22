8680d1e2d03fb5d28d6f4cbe54141ca3111f24fa
hyson666666 <2854529108@qq.cpm>
2019-12-22 10:20:10 +0800

    线程通信

---
+++ src/cn/e3mall/demo_1214/future/FutureDemo.java
@@ -0,0 +1,15 @@
+package cn.e3mall.demo_1214.future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+
+public class FutureDemo {
+
+    public static void main(String[] args) {
+//        Condition
+//        FutureTask
+    }
+
+}
