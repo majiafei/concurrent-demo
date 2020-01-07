dea5c83c362951bb0f77ea419e9f54e03728fa66
hyson666666 <2854529108@qq.cpm>
2020-01-07 21:49:33 +0800

    线程通信

--- src/cn/e3mall/demo_1214/queue/BlockingQueueTest.java
+++ src/cn/e3mall/demo_1214/queue/BlockingQueueTest.java
@@ -1,11 +1,13 @@
 package cn.e3mall.demo_1214.queue;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class BlockingQueueTest {
 
     public static void main(String[] args) {
         ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue(4);
+        LinkedBlockingQueue blockingQueue = new LinkedBlockingQueue();
     }
 
 }
