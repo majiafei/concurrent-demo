ca3f21e288246ac36538acf5cdac2b88689247cf
hyson666666 <2854529108@qq.cpm>
2020-01-07 20:20:51 +0800

    线程通信

--- src/cn/e3mall/demo_1214/future/FutureDemo.java
+++ src/cn/e3mall/demo_1214/future/FutureDemo.java
@@ -1,6 +1,8 @@
 package cn.e3mall.demo_1214.future;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
@@ -8,6 +10,7 @@ import java.util.concurrent.locks.Condition;
 public class FutureDemo {
 
     public static void main(String[] args) {
+
 //        Condition
 //        FutureTask
     }
---
+++ src/cn/e3mall/demo_1214/queue/BlockingQueueTest.java
@@ -0,0 +1,11 @@
+package cn.e3mall.demo_1214.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class BlockingQueueTest {
+
+    public static void main(String[] args) {
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue(4);
+    }
+
+}
