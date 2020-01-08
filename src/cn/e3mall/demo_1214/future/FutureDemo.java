199f0a8c504578f8fef46ce228abcb7afbf3fe59
fury36 <40300926+fury36@users.noreply.github.com>
2020-01-08 13:36:07 +0800

    敏感词算法

--- src/cn/e3mall/demo_1214/future/FutureDemo.java
+++ src/cn/e3mall/demo_1214/future/FutureDemo.java
@@ -1,5 +1,7 @@
 package cn.e3mall.demo_1214.future;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
@@ -7,9 +9,18 @@ import java.util.concurrent.locks.Condition;
 
 public class FutureDemo {
 
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
 //        Condition
 //        FutureTask
+    }*/
+
+    public static void main(String[] args) {
+        Map<Integer, String> ebayAccountMap = new HashMap<>();
+        Map<Integer, Map<Integer, String>> platformAccountMap = new HashMap<>();
+        platformAccountMap.put(new Integer(1), ebayAccountMap);
+        ebayAccountMap.put(new Integer(1), "");
+        ebayAccountMap = null;
+        System.out.println(platformAccountMap);
     }
 
 }
