da1c0738fa66c5542ca56c465ba68a0d5f3da223
fury36 <40300926+fury36@users.noreply.github.com>
2020-05-23 10:26:40 +0800

    找出第三便宜的价格

--- src/cn/e3mall/demo_1214/aqs/CyclicBarrierTest.java
+++ src/cn/e3mall/demo_1214/aqs/CyclicBarrierTest.java
@@ -1,6 +1,9 @@
 package cn.e3mall.demo_1214.aqs;
 
+import javax.swing.*;
+import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
 /**
@@ -52,6 +55,70 @@ public class CyclicBarrierTest {
             }
         }
     }*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        Scanner scanner2 = new Scanner(System.in);
+        String numStr = scanner2.nextLine();
+        String[] priceStr = numStr.split(" ");
+        if (Objects.isNull(priceStr)) {
+            System.out.println("请输入价格");
+            return;
+        }
+        if (priceStr.length != i) {
+            System.out.println("请输入" + i + "个价格");
+            return;
+        }
+
+        int[] priceArray = new int[priceStr.length];
+        for (int m = 0; m < priceStr.length; m++) {
+            try {
+                int price = Integer.valueOf(priceStr[m]);
+                if (price <= 0) {
+                    System.out.println("第" + (i + 1) + "个价格不是正整数");
+                    return;
+                }
+                if (price > 1000) {
+                    System.out.println("第" + (i + 1) + "个价格必须小于等于1000");
+                    return;
+                }
+
+                priceArray[m] = price;
+            } catch (NumberFormatException e) {
+                System.out.println("第" + (i + 1) + "个价格不是整数");
+                return;
+            }
+
+        }
+
+        int tem = 0;
+        for (int m = 0; m < priceArray.length - 1; m++) {
+            tem = priceArray[m];
+            if (priceArray[m] > priceArray[m + 1]) {
+                priceArray[m] = priceArray[m + 1];
+                priceArray[m + 1] = tem;
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int th = priceArray[0];
+        int count = 1;
+        for (int m = 1; m < priceArray.length; m++) {
+            if (priceArray[m] > th) {
+                th = priceArray[m];
+                count++;
+                if (count == 3) {
+                    break;
+                }
+            }
+        }
+        if (count == 3) {
+            System.out.println(th);
+        } else {
+            System.out.println(-1);
+        }
+    }
 
 
 
