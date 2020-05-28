a85000137a8f3beb07b52e3d93fb41bf7ad97205
fury36 <40300926+fury36@users.noreply.github.com>
2020-05-28 10:32:53 +0800

    任务框架

---
+++ src/cn/e3mall/demo_1214/taskframework/enums/ResultEnum.java
@@ -0,0 +1,37 @@
+package cn.e3mall.demo_1214.taskframework.enums;
+
+/**
+ * @ClassName: ResultEnum
+ * @Auther: admin
+ * @Date: 2020/1/15 11:10
+ * @Description:
+ */
+public enum ResultEnum {
+    SUCCESS(0, "执行成功"),
+    FAILED(1, "执行失败"),
+    EXCEPTION(2, "异常")
+    ;
+
+    private int code;
+    private String message;
+
+    ResultEnum(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }}
