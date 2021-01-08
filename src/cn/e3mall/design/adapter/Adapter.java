2682ddcf8b61f124d036afb0b301eef505bba7e9
majiafei <2630264843@qq.com>
2021-01-08 09:27:05 +0800

    Update Adapter.java

--- src/cn/e3mall/design/adapter/Adapter.java
+++ src/cn/e3mall/design/adapter/Adapter.java
@@ -1,6 +1,7 @@
 package cn.e3mall.design.adapter;
 
 /**
+VGAInterface 被转换成的目标接口
  * @ProjectName: house
  * @Package: cn.e3mall.design.adapter
  * @ClassName: Adapter
@@ -9,13 +10,15 @@ package cn.e3mall.design.adapter;
  * @Date: 2019/1/22 10:11
  */
 public class Adapter implements VGAInterface{
-
+ 
+    // 被转换的接口
     private HDMIInterface hdmiInterface;
 
     public Adapter(HDMIInterface hdmiInterface) {
         this.hdmiInterface = hdmiInterface;
     }
 
+    // 真正想要的格式
     @Override
     public void vga() {
         hdmiInterface.hdmi();
